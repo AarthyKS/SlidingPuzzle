562dd8aa703f0aaff688ff0ddc0b357c5874db77
Aarthy Kidambi Sekar <aarthy_kidambi_sekar@dell.com>
2017-07-17 16:41:56 -0500

    as/ Adding puzzle basic UI

--- SliddingPuzzle/SliddingPuzzle/MainPage.xaml.cs
+++ SliddingPuzzle/SliddingPuzzle/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.PlayTo;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,45 @@ namespace SliddingPuzzle
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Button[] buttons = new Button[16];
+        private Button empty; // references one of the buttons in `buttons`
+        private TextBlock clicksLabel;
+        private TextBox moveCountText;
+        private int moveCount = 0;
+        private Random random = new Random();
+
         public MainPage()
         {
             this.InitializeComponent();
+            InitialSetup();
         }
+
+        private void InitialSetup()
+        {
+            int current = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[current] = new Button();
+                    if (i == 3 && j == 3)
+                    {
+                        buttons[current].Content = "";
+                    }
+                    else
+                    {
+                        buttons[current].Content = current + 1;
+                    }
+                    buttons[current].Name = "btn" + current.ToString();
+                    buttons[current].Width = 100;
+                    buttons[current].Height = 100;
+                    Grid.SetRow(buttons[current],i);
+                    Grid.SetColumn(buttons[current], j);
+                    puzzleGrid.Children.Add(buttons[current]);
+                    current++;
+                }
+            }
+        }
+        
     }
 }
